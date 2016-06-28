25b4734374c4faf2f4d622b9e7f64b40615b8d19
jonathandavidblack <jonnybl915@gmail.com>
2016-06-28 12:31:34 -0400

    upload functionality

--- src/main/java/com/theironyard/controllers/IronGramRestController.java
+++ src/main/java/com/theironyard/controllers/IronGramRestController.java
@@ -1,9 +1,19 @@
 package com.theironyard.controllers;
 
+import com.sun.xml.internal.ws.api.message.ExceptionHasMessage;
+import com.theironyard.entities.Photo;
+import com.theironyard.entities.User;
 import com.theironyard.services.PhotoRepository;
 import com.theironyard.services.UserRepository;
+import com.theironyard.utilities.PasswordStorage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.security.auth.callback.PasswordCallback;
+import javax.servlet.http.HttpSession;
 
 /**
  * Created by jonathandavidblack on 6/28/16.
@@ -15,4 +25,35 @@ public class IronGramRestController {
 
     @Autowired
     PhotoRepository photos;
+
+
+    @RequestMapping(path = "/login", method = RequestMethod.POST)
+    public User login(@RequestBody User user, HttpSession session) throws Exception {
+        User userFromDB = users.findFirstByName(user.getName());
+        if (userFromDB == null) {
+            user.setPassword(PasswordStorage.createHash(user.getPassword()));
+            users.save(user);
+        }
+        else if (!PasswordStorage.verifyPassword(user.getPassword(), userFromDB.getPassword())) {
+            throw new Exception("Wrong Password!");
+        }
+        session.setAttribute("username", user.getName());
+        return user;
+    }
+
+
+    @RequestMapping(path = "/logout", method = RequestMethod.POST)
+    public void logout(HttpSession session) {
+        session.invalidate();
+    }
+
+
+    @RequestMapping(path = "/photos", method = RequestMethod.GET)
+    public Iterable<Photo> getPhotos(HttpSession session) {
+
+        String username = (String) session.getAttribute("username");
+        User user = users.findFirstByName(username);
+        return photos.findByRecipient(user);
+
+    }
 }
--- src/main/java/com/theironyard/services/PhotoRepository.java
+++ src/main/java/com/theironyard/services/PhotoRepository.java
@@ -1,10 +1,13 @@
 package com.theironyard.services;
 
 import com.theironyard.entities.Photo;
+import com.theironyard.entities.User;
 import org.springframework.data.repository.CrudRepository;
 
 /**
  * Created by jonathandavidblack on 6/28/16.
  */
 public interface PhotoRepository extends CrudRepository<Photo, Integer> {
+    public Iterable<Photo> findByRecipient(User recipient);
+
 }
